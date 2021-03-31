8d9c6f4c23d3d56300f77dfbde991f7f2ef66bf9
ProteinCODER <cocoon1121@naver.com>
2021-04-01 04:35:02 +0900

    statement

---
+++ ReJava/ch4/IfExample.java
@@ -0,0 +1,18 @@
+package ch4;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		int score = 86;
+		
+		if (score >= 90) {
+			System.out.println("우수합니다");
+		} else {
+     		System.out.println("우수하지 않군요");
+     	}
+			
+		}
+
+	}
+
+
